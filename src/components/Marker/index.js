1693e250fdf7838a8c039177e28d2be4c2192b28
Andressa Tonietto Reis <andressa@cargon.com>
2021-03-31 09:17:54 -0300

    add marker component

---
+++ src/components/Marker/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ImPowerCord } from 'react-icons/im';
+
+const Marker = () => {
+  return <ImPowerCord style={{ color: '#00BFFF', size: '30px' }}></ImPowerCord>;
+};
+
+export default Marker;
